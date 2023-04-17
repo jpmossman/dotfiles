6297f62d6954d6f421da9af01eacf67fbf662ca6
JP Mossman <jadenmossman2000@gmail.com>
2023-04-16 19:01:33 -0500

    .

--- docs/.easy-install.sh
+++ docs/.easy-install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
 sudo apt update
 sudo apt install -y curl git gpg
-sh -c "$(curl -fsLS get.chezmoi.io)"
-./bin/chezmoi init https://github.com/jpmossman/dotfiles.git
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/jpmossman/dotfiles.git
